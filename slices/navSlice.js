5c140764610b633fc811b376a1891666e6623764
Julien <julienranson77@gmail.com>
2022-08-16 22:33:17 +0200

    adding store and configure for data + Tailwind for design + test HomeScreen

---
+++ slices/navSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  // Where are you ?
+  origin: null,
+
+  // What is your destination ?
+  destination: null,
+
+  // Is explicit
+  travelTimeInformation: null
+}
+
+// Send data
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+
+// Recovery data
+
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+// Export composant
+export default navSlice.reducer;
